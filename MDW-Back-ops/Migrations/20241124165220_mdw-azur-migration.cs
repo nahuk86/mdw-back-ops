ce6e432098f3f8558a5c033d85af7e82db6e0692
Nahuel Adrian Krauss <130124513+nahuk86@users.noreply.github.com>
2024-11-24 14:06:07 -0300

    production release

---
+++ MDW-Back-ops/Migrations/20241124165220_mdw-azur-migration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MDW_Back_ops.Migrations
+{
+    /// <inheritdoc />
+    public partial class mdwazurmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
